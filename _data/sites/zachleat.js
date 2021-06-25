262a79e9d4594bf8d733172b0046d39732eee1e8
oliver41 <oliverpfeiffer41@gmail.com>
2021-06-25 19:23:46 +0200

    Update zachleat.js

--- _data/sites/zachleat.js
+++ _data/sites/zachleat.js
@@ -1,6 +1,6 @@
 module.exports = {
-	name: "zachleat.com", // optional, falls back to object key
-	description: "Zach’s Personal web site",
+	name: "eleventy-jet21.netlify.app", // optional, falls back to object key
+	description: "Jet21 test site",
 	// skip if localhost
 	// skip if this is a new fork of the speedlify (not Zach’s)
 	skip: !process.env.CONTEXT || process.env.SITE_NAME !== "speedlify",
@@ -13,17 +13,9 @@ module.exports = {
 		freshChrome: "site"
 	},
 	urls: [
-		"https://www.zachleat.com/",
-		"https://www.zachleat.com/web/",
-		"https://www.zachleat.com/web/about/",
-		"https://www.zachleat.com/web/fonts/",
-		"https://www.zachleat.com/web/eleventy/",
-		"https://www.zachleat.com/resume/",
-		"https://www.zachleat.com/twitter/",
-		// Popular Posts
-		"https://www.zachleat.com/web/lighthouse-in-footer/",
-		"https://www.zachleat.com/web/speedlify/",
-		"https://www.zachleat.com/web/comprehensive-webfonts/",
-		"https://www.zachleat.com/web/google-fonts-display/",
+		"https://eleventy-jet21.netlify.app/",
+		"https://eleventy-jet21.netlify.app/playground1/",
+		"https://eleventy-jet21.netlify.app/newsletter/",
+		"https://eleventy-jet21.netlify.app/twsite01/",
 	]
 };
